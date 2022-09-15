e6433bc02eae49c59b0bd68f07f4f54d2ff81035
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-09-15 17:37:50 +0500

    Added Next Prime Number problem

---
+++ NextPrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masala
+{
+    public class NextPrimeNumber
+    {
+        public static void NextPrNumber(string[] args)
+        {
+            int a = 15;
+            while(!ToqSon(a))
+            {
+                a++;
+            }
+            Console.WriteLine(a);
+        }
+
+
+        static bool ToqSon(int a)
+        {
+            bool check = true;
+            for (int i = 2; i <= Math.Sqrt(a); i++)
+            {
+                if (a % i == 0)
+                    check = false;
+            }
+
+            return check;
+    }
+}
+}
--- Program.cs
+++ Program.cs
@@ -12,3 +12,4 @@ static int Max(int a, int b)
 {
     return a > b ? a : b;
 }
+
