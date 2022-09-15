87a389c10c02feae131ccad64fb4624fa3fd9640
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-09-15 17:17:48 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int[] nums = { 1, 1, 1, 1 };
+int[] tmp = new int[nums.Length + 1];
+tmp[0] = 0;
+tmp[1] = nums[0];
+for(int i = 1; i < nums.Length; i++)
+{
+    tmp[i+1] = Max(tmp[i], tmp[i-1] + nums[i]);
+}
+return tmp[nums.Length];
+
+static int Max(int a, int b)
+{
+    return a > b ? a : b;
+}
