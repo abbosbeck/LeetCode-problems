b1c3acf551cf9093c59a05350e8dd351b0168b03
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-09-16 20:33:00 +0500

    Added Climbing Staires problem

---
+++ 70 Climbing Stairs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masala
+{
+    public class Fib
+    {
+        public static int Fib(int n)
+        {
+            if (n < 3)
+            {
+                return n;
+            }
+            int a = 1;
+            int b = 2;
+            for (int i = 2; i < n; i++)
+            {
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return b;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
-﻿int[] nums = { 1, 1, 1, 1 };
+﻿using Masala;
+
+Console.WriteLine(Fib.Fib(5));
+
+/*int n = 5;
+int counter = 0;
+
+static int Sum(int a)
+{
+    if (a <= 1)
+        return a;
+    return Sum(a - 1) + Sum(a - 2);
+}
+*/
+
+/*int[] nums = { 1, 1, 1, 1 };
 int[] tmp = new int[nums.Length + 1];
 tmp[0] = 0;
 tmp[1] = nums[0];
@@ -13,3 +28,4 @@ static int Max(int a, int b)
     return a > b ? a : b;
 }
 
+*/
