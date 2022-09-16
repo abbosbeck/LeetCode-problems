952390452e218062d7c8c40f6bc65555b58fd357
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-09-16 09:48:51 +0500

    added 121-problem

---
+++ 121 Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,21 @@
+﻿//121. Best Time to Buy and Sell Stock 
+// Type: easy
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int max = 0;
+        int min = prices[0];
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] < min)
+                min = prices[i];
+            else if (prices[i] - min > max)
+            {
+                max = prices[i] - min;
+            }
+        }
+        return max;
+    }
+}
