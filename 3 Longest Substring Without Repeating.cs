22e2034cf77b5e07e9f156b3f5dd2c65b0cc19c0
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-10-06 09:31:30 +0500

    added problem number 3

---
+++ 3 Longest Substring Without Repeating.cs
@@ -0,0 +1,39 @@
+﻿public class Leng
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (s != null)
+        {
+            int longestLength = 0;
+
+            string curr = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                curr = s[i].ToString();
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (!curr.Contains(s[j]))
+                    {
+                        curr = curr + s[j].ToString();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (curr.Length > longestLength)
+                    longestLength = curr.Length;
+
+            }
+            if (curr.Length > longestLength)
+                longestLength = curr.Length;
+
+            return longestLength;
+        }
+        return 0;
+    }
+}
+
+
+//Console.WriteLine(_231_Power_of_Two.Checker(16));
+//Console.WriteLine(Fib.Fibon(5));
--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 ﻿using Masala;
 
-Console.WriteLine(_231_Power_of_Two.Checker(16));
- 
 
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "pwwkew";
+        Leng number = new Leng();
+        Console.WriteLine(number.LengthOfLongestSubstring(text));
+    }
+}
+
+
+//Console.WriteLine(_231_Power_of_Two.Checker(16));
 //Console.WriteLine(Fib.Fibon(5));
