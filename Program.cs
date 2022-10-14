a4de5b9f0e9f516c9d36a9a5547a8bade5ca678c
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-10-14 11:04:53 +0500

    added roman to integar

--- Program.cs
+++ Program.cs
@@ -1,16 +1,11 @@
 ﻿using Masala;
-
+using System.Collections;
+using System.Text;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        string text = "pwwkew";
-        Leng number = new Leng();
-        Console.WriteLine(number.LengthOfLongestSubstring(text));
     }
+
 }
-
-
-//Console.WriteLine(_231_Power_of_Two.Checker(16));
-//Console.WriteLine(Fib.Fibon(5));
---
+++ Roman to Integar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Hashtable numbers = new Hashtable()
+        {
+            {'I', 1 },
+            {'V', 5 },
+            {'X', 10 },
+            {'L', 50 },
+            {'C', 100 },
+            {'D', 500 },
+            {'M', 1000 }
+        };
+        int result = 0;
+        int length = s.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (i != s.Length - 1 && ((s[i] == 'I' && (s[i + 1] == 'V' || s[i + 1] == 'X'))
+                || (s[i] == 'X' && (s[i + 1] == 'L' || s[i + 1] == 'C'))
+                || (s[i] == 'C') && (s[i + 1] == 'D' || s[i + 1] == 'M')))
+            {
+                result += Convert.ToInt32(numbers[s[i + 1]]);
+                result -= Convert.ToInt32(numbers[s[i]]);
+                i++;
+            }
+            else
+                result += Convert.ToInt32(numbers[s[i]]);
+        };
+        return result;
+    }
+}
