c2a2e0f374047904b5377bd01fbdaa71f2548f21
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-10-28 15:28:24 +0500

    added MaximumWealth problem

---
+++ MaximumWealthProblem.cs
@@ -0,0 +1,15 @@
+﻿public class MaximumWealthProblem
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        int sum = accounts[0].Sum();
+        for(int i = 1; i < accounts.Length; i++)
+        {
+            if(accounts[i].Sum() > sum)
+                sum = accounts[i].Sum();
+        }
+        return sum;
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -6,12 +6,10 @@ public class Program
 {
     static void Main(string[] args)
     {
-        string[] firmalar = { "Phone Sony", "iPhone", "TV Sony" };
-        var problem = new MinimumSumOfFourDigitNumberAfterSplittingDigits();
-        Console.WriteLine(problem.MinimumSum(2932));
+        string firmalar = "is2 sentence4 This1 a3";
+        
     }
 
-    
 }
 
 
