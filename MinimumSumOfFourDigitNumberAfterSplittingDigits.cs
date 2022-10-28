645f5a128f437f2d48381fa4b8dacbbb03327362
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-10-28 09:57:48 +0500

    added Minimum Sum of Four Digit Number After Splitting Digits 2160

---
+++ MinimumSumOfFourDigitNumberAfterSplittingDigits.cs
@@ -0,0 +1,16 @@
+﻿public class MinimumSumOfFourDigitNumberAfterSplittingDigits
+{
+    public int MinimumSum(int num)
+    {
+        string number = num.ToString();
+        char[] charNum = new char[number.Length];
+        for(int i = 0; i < number.Length; i++)
+        {
+            charNum[i] = number[i];
+        }
+
+        return ((charNum[0] - '0') * 10 + charNum[2] - '0' + ((charNum[1] - '0') * 10 + charNum[3] - '0'));
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿using Masala;
 using System.Collections;
+using System.Text;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        
+        string[] firmalar = { "Phone Sony", "iPhone", "TV Sony" };
+        var problem = new MinimumSumOfFourDigitNumberAfterSplittingDigits();
+        Console.WriteLine(problem.MinimumSum(2932));
     }
+
+    
 }
+
+
