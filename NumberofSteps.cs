4d1afdf458bc5d3c1dbeff8d92ac4d636f3cebb4
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-12-11 11:49:53 +0500

    added NumberofSteps

---
+++ NumberofSteps.cs
@@ -0,0 +1,22 @@
+﻿public class NumberofSteps
+{
+    public int NumberOfSteps(int num)
+    {
+        int result = 0;
+        while(num > 0)
+        {
+            if(num % 2== 0)
+            {
+                result++;
+                num /= 2;
+            }
+            else
+            {
+                result++;
+                num -= 1;
+            }
+
+        }
+        return result;
+    }
+}
