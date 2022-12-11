9a4955426d0dc3f3aa73cdf6ce766f0384efa1df
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-12-11 15:08:30 +0500

    added one problem

---
+++ AverageSalaryExcludingtheMinimumandMaximumSalary.cs
@@ -0,0 +1,13 @@
+﻿public static class AverageSalaryExcludingtheMinimumandMaximumSalary
+{
+    public static double Average(int[] salary)
+    {
+        double result = 0;
+        Array.Sort(salary);
+        for(int i = 1; i < salary.Length - 1; i++)
+        {
+            result += salary[i];
+        }
+        return result / (salary.Length - 2);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,6 @@
 {
     public static void Main(string[] args)
     {
-        foreach(var a in SelfDividingNumberss.SelfDividingNumbers(1, 22))
-        {
-            Console.WriteLine(a);
-        };
+        Console.WriteLine(AverageSalaryExcludingtheMinimumandMaximumSalary.Average(new int[] { 48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000 }));
     }
 }
