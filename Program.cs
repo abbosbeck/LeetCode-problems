aabd9dd6522193890ef3ab2d39cfc9b9af40eeb7
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-12-11 15:10:50 +0500

    added 53 th problem

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(AverageSalaryExcludingtheMinimumandMaximumSalary.Average(new int[] { 48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000 }));
+        Console.WriteLine(MaximumSubarray.MaxSubArray(new int[] { 48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000 }));
     }
 }
