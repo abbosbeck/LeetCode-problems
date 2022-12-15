e936ed3fca90c69b276879f60fff2890e3c39e3e
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-12-15 18:40:30 +0500

    added one problem

---
+++ ArrayPartition.cs
@@ -0,0 +1,13 @@
+﻿public static class ArrayPartition
+{
+    public static int ArrayPairSum(int[] nums)
+    {
+        int sum = 0;
+        Array.Sort(nums);
+        for(int i = 0; i < nums.Length; i += 2)
+        {
+            sum += nums[i];
+        }
+        return sum; 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(MaximumSubarray.MaxSubArray(new int[] { 48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000 }));
+        Console.WriteLine(ArrayPartition.ArrayPairSum(new int[] { 6, 2, 6, 5, 1, 2 }));   
     }
 }
