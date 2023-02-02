d7a96b87185fed31d6489089efde08efa950aa67
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-02 10:25:13 +0500

    9. Palindrome Number problem added

--- Program.cs
+++ Program.cs
@@ -2,9 +2,7 @@
 {
     public static void Main(string[] args)
     {
-        foreach(int a in SortArrayByParityClass.SortArrayByParity(new int[] { 6, 2, 6, 5, 1, 2 }))
-        {
-            Console.WriteLine(a);
-        }
+        var solution = new SolutionIsPalindrome();
+        Console.WriteLine(solution.IsPalindrome(121));
     }
 }
---
+++ SolutionIsPalindrome.cs
@@ -0,0 +1,15 @@
+﻿public class SolutionIsPalindrome
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+        int firstNumber = x;
+        int newNumber = 0;
+        while (x > 0)
+        {
+            newNumber = newNumber * 10 + (x % 10);
+            x /= 10;
+        }
+        return firstNumber == newNumber;
+    }
+}
